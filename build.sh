756ac13870b320c4def9e55c794cfce1bdd53ef9
Christopher Mårtensson <cribalik@gmail.com>
2018-03-14 00:25:28 +1000

    added g++ warnings

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 # RELEASE_FLAGS="-O3"
-g++ mineclone.cpp -std=c++11 -g ${RELEASE_FLAGS} -ffast-math -Iinclude -L. -lGL -lSDL2 -ldl #-fsanitize=address
+g++ mineclone.cpp -Wall -Wno-unused-function -Wno-unused-but-set-variable -std=c++11 -g ${RELEASE_FLAGS} -ffast-math -Iinclude -L. -lGL -lSDL2 -ldl -fsanitize=address
