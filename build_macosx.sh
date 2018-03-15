b9d63439f3a5544e46f182d797535e713ecbdeef
Kaan Karaoglu <kaankaraoglu@outlook.com>
2018-03-15 19:30:58 +0100

    Added verbose to macOS build script

--- build_macosx.sh
+++ build_macosx.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 # RELEASE_FLAGS="-O3"
-g++ mineclone.cpp -std=c++11 -g ${RELEASE_FLAGS} -ffast-math -Iinclude -L. -lSDL2 -ldl -fsanitize=address -framework CoreFoundation
+g++ mineclone.cpp -v -std=c++11 -g ${RELEASE_FLAGS} -ffast-math -Iinclude -L. -lSDL2 -ldl -fsanitize=address -framework CoreFoundation
