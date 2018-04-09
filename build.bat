9524968fb5dd849d16e51a811c1a49fe14d78dfb
Christopher Mårtensson <cmarte@kth.se>
2018-04-09 13:06:56 +1000

    Add Ox to build script

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 rem -Ox -RTCsu 
 rem openvr_api.lib
-cl mineclone.cpp -I.\include -W3 -ZI -link -out:mineclone.exe opengl32.lib SDL2.lib -debug
+cl mineclone.cpp -Ox -I.\include -W3 -ZI -link -out:mineclone.exe opengl32.lib SDL2.lib -debug
