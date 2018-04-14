cd8b438c4a835bbfc16b2b694b6effa840503bfa
Christopher Mårtensson <cribalik@gmail.com>
2018-04-14 15:02:00 +1000

    Added gcc sanitizers for undefined behavior

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # RELEASE_FLAGS="-O3"
-g++ mineclone.cpp -o mineclone.out -Wall -Wno-unused-function -Wno-unused-but-set-variable -std=c++11 -g ${RELEASE_FLAGS} -ffast-math -Iinclude -L. -lGL -lSDL2 -ldl #-fsanitize=address
+g++ mineclone.cpp -o mineclone.out -Wall -Wextra -Wno-unused-function -Wno-unused-but-set-variable -std=c++11 -g ${RELEASE_FLAGS} -ffast-math -Iinclude -L. -lGL -lSDL2 -ldl -fno-sanitize-recover # -fsanitize=undefined -fsanitize=address
